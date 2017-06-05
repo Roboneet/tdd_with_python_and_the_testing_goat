8fe29ae1af4ca1317ae03d0c1d08afe7556a4f34
Roboneet <neethumariyajoy@gmail.com>
2017-06-05 17:34:33 +0530

    test1: checking page title

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,31 @@
 from selenium import webdriver
+import unittest
 
-chrome_path = r"/Users/joyp.isahac/Downloads/chromedriver"
+# chrome_path = r"/Users/joyp.isahac/Downloads/chromedriver"
 
-browser = webdriver.Chrome(chrome_path)
+# browser = webdriver.Chrome(chrome_path)
 
-browser.get('http://localhost:8000')
+# browser.get('http://localhost:8000')
+# assert 'To-Do' in browser.title, "Browser Title was "+browser.title
 
-assert 'Django' in browser.title
+# browser.quit()
+
+class  MyTests(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome(chrome_path)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_to_start_a_list_and_retrive_it_later(self):
+
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do', self.browser.title)
+		self.fail("Finish Test")
+
+		self.browser.quit()
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
