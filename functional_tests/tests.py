19cd3bc54a3070c7590fe16692f1f0859be5f4eb
Roboneet <neethumariyajoy@gmail.com>
2017-06-09 20:54:38 +0530

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -13,7 +13,7 @@ chrome_path = r"/Users/joyp.isahac/Downloads/chromedriver"
 
 # browser.quit()
 
-class  MyTests(LiveServerTestCase):
+class  NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome(chrome_path)
@@ -76,7 +76,23 @@ class  MyTests(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
-
-		self.fail("Finish Test")
 		self.browser.quit()
 
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width']/2,
+			512,
+			delta=5
+			)
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width']/2,
+			512,
+			delta=5
+			)
+		
--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^/new$', views.new_list, name='new_list'),
-    url(r'^/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
