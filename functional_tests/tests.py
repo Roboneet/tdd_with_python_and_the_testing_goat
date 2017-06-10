3e5ef4a2d62b86199a4dce564c81d25061a7a844
Roboneet <neethumariyajoy@gmail.com>
2017-06-10 15:37:43 +0530

    used bootstrap to imporove layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -13,7 +13,7 @@ chrome_path = r"/Users/joyp.isahac/Downloads/chromedriver"
 
 # browser.quit()
 
-class  NewVisitorTest(LiveServerTestCase):
+class  NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome(chrome_path)
